b7804678c540a7359edf586eda6e42fad61b3e28
iftekher2108 <parvezkhan21082002@gmail.com>
2024-04-21 17:08:48 +0600

    mejor ui change

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,8 @@
 
-
+$(document).ready(function(){
 $('.select_all').click(function() {
     $('.ids').prop('checked',$(this).prop('checked'))
 })
-
 
 $('.delete-btn').click(function() {
     // $('.ids:checked').hide(300)
@@ -11,20 +10,36 @@ $('.delete-btn').click(function() {
     // console.log('delete')
 })
 
-
 $(".add-btn").click(function(){
   
     $('.note').append( 
             `<tr style='text-align:center;'>
                 <td><input type="checkbox" class="ids"></td>
                 
-                <td>`+$('.text-input').val()+`</td>
+                <td class='text-edit' title='double click to edit'>`+$('.text-input').val()+`</td>
+                <td class='text-edit'><button class="del-btn">Delete</button></td>
             </tr>`
             );
 
             $('.text-input').val("");
-              
+        
 })
+
+$(document).on('click', '.del-btn', function() {
+    $(this).parent().parent('tr').hide(300);
+});
+
+
+$(document).on('click', '.text-edit', function() {
+    $(this).attr('contenteditable', true);
+});
+
+$(document).on('blur', '.text-edit', function() {
+    $(this).attr('contenteditable', false);
+});
+
+});
+
 
 
 
